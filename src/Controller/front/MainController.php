b1f00fb7997086fb0f07cdf9d1160aeacff8d822
soniahammou <sonia.hammou@oclock.school>
2024-07-11 21:01:59 +0200

    feat/responsive design

--- src/Controller/front/MainController.php
+++ src/Controller/front/MainController.php
@@ -10,22 +10,20 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/', name: 'app_main')]
+    #[Route('/', name: 'app_main_front')]
     public function index(ProjectRepository $projectRepository): Response
     {
-        $this->denyAccessUnlessGranted('ROLE_USER');
-
+        // $this->denyAccessUnlessGranted('ROLE_USER');
 
 
         $projects = $projectRepository->findBy(
             [],
             ['created_at' => 'DESC'],
-            4 // Limite les résultats à 4       
+            //4 // Limite les résultats à 4       
          );
   
 
         return $this->render('front/main/index.html.twig', [
-            'controller_name' => 'MainController',
             'projects'=>$projects,
         ]);
     }
